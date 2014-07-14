ac03bf13d864a806b990f64b46183eef5fda7129
Toby Knott <codeconcussion@gmail.com>
2014-07-14 15:57:20 -0500

    downgrade to 4.0 for xp support

--- CodeConcussion.KVL/Utility/AppBootstrapper.cs
+++ CodeConcussion.KVL/Utility/AppBootstrapper.cs
@@ -15,11 +15,11 @@ namespace CodeConcussion.KVL.Utility
         {
             DisplayRootViewFor<ShellViewModel>();
 
-            //Application.Current.MainWindow.SizeToContent = SizeToContent.Manual;
-            //Application.Current.MainWindow.Width = (SystemParameters.PrimaryScreenWidth / 3d) * 2;
-            //Application.Current.MainWindow.Height = (SystemParameters.PrimaryScreenHeight / 3d) * 2;
-            //Application.Current.MainWindow.Left = SystemParameters.PrimaryScreenWidth / 6d;
-            //Application.Current.MainWindow.Top = SystemParameters.PrimaryScreenHeight / 6d;
+            Application.Current.MainWindow.SizeToContent = SizeToContent.Manual;
+            Application.Current.MainWindow.Width = (SystemParameters.PrimaryScreenWidth * .75);
+            Application.Current.MainWindow.Height = (SystemParameters.PrimaryScreenHeight * .75);
+            Application.Current.MainWindow.Left = (SystemParameters.PrimaryScreenWidth * .125);
+            Application.Current.MainWindow.Top = (SystemParameters.PrimaryScreenHeight * .125);
 
             //Application.Current.MainWindow.SizeToContent = SizeToContent.Manual;
             //Application.Current.MainWindow.Width = 500;
